4838ce614ebbfeddb659bcd89f5874cf3014c3d5
Anton Kuznetcov <info@direct-dev.ru>
2023-07-09 20:31:20 +0600

    litle commit

--- setenvvars.sh
+++ setenvvars.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-export DOCKER_TOKEN=$(kubectl get secret dockerhub-token -n test-app -o jsonpath='{.data.token}' | base64 --decode)
-export DOCKER_USER=$(kubectl get secret dockerhub-token -n test-app -o jsonpath='{.data.username}' | base64 --decode)
-export DOCKER_SERVER=$(kubectl get secret dockerhub-token -n test-app -o jsonpath='{.data.server}' | base64 --decode)
+DOCKER_TOKEN=$(kubectl get secret dockerhub-token -n test-app -o jsonpath='{.data.token}' | base64 --decode)
+DOCKER_USER=$(kubectl get secret dockerhub-token -n test-app -o jsonpath='{.data.username}' | base64 --decode)
+DOCKER_SERVER=$(kubectl get secret dockerhub-token -n test-app -o jsonpath='{.data.server}' | base64 --decode)
+
+export DOCKER_TOKEN
+export DOCKER_USER
+export DOCKER_SERVER
